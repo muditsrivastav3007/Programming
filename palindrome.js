4e858b124621d60a62ce1ec68177dd75d4232ffd
Mudit Srivastav <mudit.srivastav@bookmyshow.com>
2020-07-11 13:47:10 +0530

    Revert "Javascript practice"
    This reverts commit c26388ee32a01cd7400c715196b707c7598c8978.

--- palindrome.js
+++
@@ -1,9 +0,0 @@
-var isPalindrome = function(x) {
-    const str = x.toString();
-    const rev = str.split('').reverse().join('');
-    if (str === rev) {
-        return true;
-    } else {
-        return false;
-    }
-};
